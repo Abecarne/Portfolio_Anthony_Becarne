8e3eb70d1f74eb831ccc45261b79c6d72f57c0f2
Anthony Becarne <anthony.becarne@gmail.com>
2024-06-17 14:28:57 +0900

    [+] Added form sending with formSpree

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -25,7 +25,7 @@ const infos = [
   {
     icon: <FaEnvelope />,
     title: "Email",
-    description: "youremail@gmail.com",
+    description: "anthony.becarne@gmail.com",
   },
   {
     icon: <FaMapMarkerAlt />,
@@ -50,7 +50,11 @@ const Contacts = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* Form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              action="https://formspree.io/f/mwkggyda"
+              method="POST"
+            >
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
@@ -59,31 +63,33 @@ const Contacts = () => {
                 assumenda aspernatur quasi veniam cum?
               </p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email address" />
-                <Input type="phone" placeholder="Phone number" />
+                <Input name="firstname" type="text" placeholder="Firstname" />
+                <Input name="lastname" type="text" placeholder="Lastname" />
+                <Input name="email" type="email" placeholder="Email address" />
+                <Input name="phone" type="tel" placeholder="Phone number" />
               </div>
 
-              <Select>
+              <Select name="service">
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="est">Web Development</SelectItem>
-                    <SelectItem value="cst">UI/UX Design</SelectItem>
-                    <SelectItem value="mst">Software Development</SelectItem>
+                    <SelectItem value="Web Development">Web Development</SelectItem>
+                    <SelectItem value="UI/UX Design">UI/UX Design</SelectItem>
+                    <SelectItem value="Software Development">Software Development</SelectItem>
                   </SelectGroup>
                 </SelectContent>
               </Select>
 
               <Textarea
+                name="message"
+                type="text"
                 className="h-[200px]"
                 placeholder="Type your message here."
               />
-              <Button size="md" className="max-w-40">
+              <Button type="submit" size="md" className="max-w-40">
                 Send Message
               </Button>
             </form>
